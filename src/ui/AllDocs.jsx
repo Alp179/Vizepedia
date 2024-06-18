9a1880264e00dd756c643c07fcdb7c91dc144ec7
Alp179 <alptekinyemenici@gmail.com>
2024-06-18 12:26:43 +0300

    MobileMenu.jsx de tüm belgeler butonu aktif edildi

--- src/ui/AllDocs.jsx
+++ src/ui/AllDocs.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import { getCurrentUser } from "../services/apiAuth";
 import { useQuery } from "@tanstack/react-query";
 import { fetchUserSelectionsDash } from "../utils/userSelectionsFetch";
@@ -6,11 +6,10 @@ import { getDocumentsForSelections } from "../utils/documentsFilter";
 import { fetchDocumentDetails } from "../utils/documentFetch";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelectedDocument } from "../context/SelectedDocumentContext";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { useDocuments } from "../context/DocumentsContext";
 import Spinner from "./Spinner";
 import { fetchCompletedDocuments } from "../utils/supabaseActions";
-import { DarkModeContext } from "../context/DarkModeContext"; // DarkModeContext'i import et
 
 const DocumentItem = styled.li`
   border-radius: 16px;
@@ -21,26 +20,6 @@ const DocumentItem = styled.li`
   &:hover {
     background-color: ${(props) => (props.isCompleted ? "#cde0d9" : "#f0f0f0")};
   }
-
-  ${({ darkMode }) =>
-    darkMode &&
-    css`
-      background-color: ${(props) => (props.isCompleted ? "#005f2f" : "none")};
-      &:hover {
-        background-color: ${(props) =>
-          props.isCompleted ? "#113322" : "#444444"};
-      }
-    `}
-`;
-
-const AllDocsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 6px;
-  background-color: ${({ darkMode }) => (darkMode ? "#222222" : "#ffffff")};
-  color: ${({ darkMode }) => (darkMode ? "#ffffff" : "#000000")};
-  padding: 20px;
-  border-radius: 8px;
 `;
 
 function AllDocs() {
@@ -52,7 +31,6 @@ function AllDocs() {
     state: { completedDocuments },
     dispatch,
   } = useDocuments();
-  const { isDarkMode } = useContext(DarkModeContext); // Dark mode bilgisini al
 
   useEffect(() => {
     getCurrentUser().then((user) => {
@@ -113,7 +91,7 @@ function AllDocs() {
   };
 
   return (
-    <AllDocsContainer darkMode={isDarkMode}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "6px" }}>
       <h2>Tüm Belgeler</h2>
       <div>Başvurunuzda gerekli olan tüm belgeler</div>
       <ul>
@@ -122,13 +100,12 @@ function AllDocs() {
             key={document.id}
             isCompleted={completedDocuments[applicationId]?.[document.docName]}
             onClick={() => handleDocumentClick(document)}
-            darkMode={isDarkMode}
           >
             {document.docName}
           </DocumentItem>
         ))}
       </ul>
-    </AllDocsContainer>
+    </div>
   );
 }
 
