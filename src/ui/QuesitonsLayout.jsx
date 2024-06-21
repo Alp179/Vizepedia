b2c902d356127773c4ff8ed6c9b9ad1d66674486
Alp179 <87341325+Alp179@users.noreply.github.com>
2024-06-21 14:49:18 +0000

    Progress bar eklendi

--- src/ui/QuesitonsLayout.jsx
+++ src/ui/QuesitonsLayout.jsx
@@ -1,8 +1,9 @@
-import styled from "styled-components";
-import { Outlet } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import MobileMenu from "./MobileMenu";
 import BackButton from "./BackButton";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -18,14 +19,68 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
+const ProgressBarContainer = styled.div`
+  width: 80%; /* Progress bar uzunluğunu %20 kısalttık */
+  padding: 1rem;
+  margin: 0 auto;
+  position: relative;
+`;
+
+const ProgressBarWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledProgressBar = styled(ProgressBar)`
+  border-radius: 50px; /* Köşeleri yuvarlatmak için */
+`;
+
+const progressAnimation = keyframes`
+  0% { width: 0; }
+  100% { width: 100%; }
+`;
+
+const ProgressBarReflection = styled.div`
+  position: absolute;
+  top: 5px; /* Çizginin barın üstünde konumlanmasını sağlar (2px + 4px) */
+  left: 15px; /* Soldan içeri girme miktarı */
+  height: 6px; /* Çizginin kalınlığı */
+  border-radius: 50px;
+  background-color: rgba(255, 255, 255, 0.5); /* Yansıma efekti için açık renk */
+  z-index: 1; /* Çizginin barın üstünde yer almasını sağlar */
+  transition: width 1s ease-in-out;
+  animation: ${progressAnimation} 1s ease-in-out;
+`;
+
 function QuestionsLayout() {
-  // const navigate = useNavigate();
+  const location = useLocation();
+  const progressValues = {
+    "/wellcome": 0,
+    "/wellcome-1": 20,
+    "/wellcome-2": 20,
+    "/wellcome-3": 40,
+    "/wellcome-4": 60,
+    "/wellcome-5": 90,
+  };
+
+  const progress = progressValues[location.pathname] || 0;
 
   return (
     <LoginLayout>
       <BackButton>Geri Dön</BackButton>
       <MobileMenu />
       <Header />
+      <ProgressBarContainer>
+        <ProgressBarWrapper>
+          <StyledProgressBar
+            completed={progress}
+            bgColor="#00FFA2"
+            baseBgColor="#64B1AD"
+            height="20px"
+            isLabelVisible={false} // Yüzdeyi kaldırdık
+          />
+          <ProgressBarReflection style={{ width: `calc(${progress}% - 30px)` }} />
+        </ProgressBarWrapper>
+      </ProgressBarContainer>
       <Container>
         <Outlet />
       </Container>
