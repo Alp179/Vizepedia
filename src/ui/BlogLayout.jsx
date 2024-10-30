a7de6b2f4d52a5ea1ac6e1253f8aa98c3d20700c
wege61 <zgnege@gmail.com>
2024-10-30 18:39:55 +0300

    aurora borealis updateinho cokarelli

--- src/ui/BlogLayout.jsx
+++ src/ui/BlogLayout.jsx
@@ -7,7 +7,7 @@ const BackgroundColor = styled.div`
   width: 100vw;
   height: 100vh;
   overflow-x: hidden;
-  position: relative; /* Üst katmanın konumlandırmasını sağlamak için relative */
+  position: relative;
 `;
 
 const auroraAnimation = keyframes`
@@ -23,35 +23,47 @@ const auroraAnimation = keyframes`
 `;
 
 const StyledBlogLayout = styled.div`
-  z-index: 0; /* Arka plan renginin üzerinde olacak şekilde z-index ayarı */
+  z-index: 0;
   position: absolute;
   top: 0;
   inset: -10px;
   left: 0;
-  filter: blur(10px);
-  opacity: 0.6;
+  filter: blur(20px);
+  opacity: 0.7;
   width: 100vw;
-  height: 400px; /* Ekranın üst %20'sini kaplayacak */
+  height: 600px;
+  @media (max-width: 850px) {
+    height: 50vh;
+  }
+  @media (max-width: 450px) {
+    height: 35vh;
+  }
   background-image: repeating-linear-gradient(
-      100deg,
-      rgba(255, 255, 255, 0.7) 0%,
-      rgba(255, 255, 255, 0.7) 7%,
-      transparent 10%,
-      transparent 12%,
-      rgba(255, 255, 255, 0.7) 16%
+      120deg,
+      rgba(248, 24, 225, 0.6) 0%,       /* #F818E1 */
+      rgba(248, 24, 225, 0.6) 7%,
+      rgba(0, 68, 102, 0.6) 10%,        /* #004466 */
+      rgba(0, 68, 102, 0.6) 12%,
+      rgba(0, 255, 162, 0.6) 16%,       /* #00FFA2 */
+      rgba(0, 255, 162, 0.6) 20%,
+      rgba(248, 24, 225, 0.6) 25%       /* #F818E1 */
     ),
     repeating-linear-gradient(
-      100deg,
-      #3b82f6 10%,
-      #818cf8 15%,
-      #60a5fa 20%,
-      #a5b4fc 25%,
-      #2563eb 30%
+      120deg,
+      #004466 5%,                        /* #004466 */
+      #00FFA2 10%,                       /* #00FFA2 */
+      #F818E1 20%,                       /* #F818E1 */
+      #004466 30%,                       /* #004466 */
+      #00FFA2 40%                        /* #00FFA2 */
     );
   background-size: 300%, 200%;
   background-position: 50% 0;
-  animation: ${auroraAnimation} 30s infinite linear;
+  animation: ${auroraAnimation} 50s infinite linear;
   pointer-events: none;
+
+  /* Alt kısımda şeffaf geçiş sağlayan maskeleme */
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+  -webkit-mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
 `;
 
 function BlogLayout() {
