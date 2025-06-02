aae12f08ca2fca1137f28c33cb684ffdbc8434fe
wege61 <zgnege@gmail.com>
2025-06-03 00:04:25 +0300

    davetiye page created

--- src/pages/Davetiye.jsx
+++ src/pages/Davetiye.jsx
@@ -1,10 +1,21 @@
 import MainPageHeader from "../ui/MainPageHeader";
+import styled from "styled-components";
+import Footer from "../ui/Footer";
+import SlideShow from "../ui/SlideShow";
+
+const Fullpage = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: var(--color-grey-1);
+`;
 
 function Davetiye() {
   return (
-    <>
+    <Fullpage>
       <MainPageHeader />
-    </>
+      <SlideShow />
+      <Footer />
+    </Fullpage>
   );
 }
 
